a08a8265a091496cd10776d806628c2ff98eb955
teerawut <teerawut@depth1st.com>
2020-02-10 16:03:46 +0700

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,7 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-app/'
     : '/',
-  "extends": "eslint:recommended",
-  "rules": {
-    "no-console": "error",
-  }
+  rules: {
+    'no-console': 'off',
+  },
 }
